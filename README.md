2db2903a61fba356bca2915c61999637ed6d6829
Vaibhav Kaushik <68017022+vansul@users.noreply.github.com>
2020-10-14 22:58:42 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The task given on day 03
+
+This is the task done by Vaibhav Kaushik which was given on Day 3 of Online Short Term Course on Full Stack Development using MERN.
---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+
+// Encoding the request body using body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add Product page
+app.get('/addProduct', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'addProduct.html'));
+});
+
+// Process the post request
+app.post('/addProduct', (req, res) => {
+  // Initialize the product object
+  const product = {
+    name: req.body.name,
+    brand: req.body.brand,
+    price: req.body.price,
+    desc: req.body.desc,
+  };
+
+  fs.readFile('products.json', 'utf8', (err, data) => {
+    // If got error then just throw it
+    if (err) throw err;
+
+    // Parse the products.json and store it inside a variable products
+    const products = JSON.parse(data);
+
+    // Push the new Product we got
+    products.push(product);
+
+    // Convert products array to JSON
+    const payload = JSON.stringify(products, null, 2) + '\n';
+
+    console.log(payload);
+
+    // Write the products Array to the products.json
+    fs.writeFile('products.json', payload, (err) => {
+      // If got error then just throw it
+      if (err) throw err;
+
+      res.status(201).json({
+        success: true,
+        product,
+      });
+    });
+  });
+});
+
+// Sending JSON of products
+app.get('/products', (_req, res) => {
+  fs.readFile('products.json', 'utf8', (err, data) => {
+    // If got error then just throw it
+    if (err) throw err;
+
+    // Return the products
+    res.json(JSON.parse(data));
+  });
+});
+
+// Showing Home Page
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Sending 404 on all routes
+app.get('*', (_req, res) => {
+  // Send the 404 status
+  res.sendStatus(404);
+});
+
+app.listen(5000);
